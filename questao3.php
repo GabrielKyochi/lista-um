31bc70d0d41d5a4ef9c659db02ac29677aee5e1f
GabrielKyochi <gabriel_k_bernardes@estudante.sesisenai.org.br>
2025-07-10 09:29:15 -0300

    Realizado a questão 3 da lista de exercícios.

---
+++ questao3.php
@@ -0,0 +1,32 @@
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Questão 3 - PHP</title>
+</head>
+<body>
+    <form method="POST" action="">
+    <label for="numero_tipo">Escreva um número para verificar se ele é positivo, negativo ou zero:</label>
+    <input type="number" id="numero_tipo" name="numero_tipo">
+    <button name="verificar_tipo">Enviar</button>
+    </form>
+
+    <?php
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        
+              if(isset($_POST['verificar_tipo'])){
+                $numero = $_POST['numero_tipo'];
+
+                if($numero > 0){
+                    echo"O número $numero é positivo!";
+                }else if($numero < 0){
+                    echo"O número $numero é negativo!";
+                }else{
+                    echo"O número $numero é 0!"; 
+                }
+            }
+        }
+    ?>
+    
+</body>
+</html>
